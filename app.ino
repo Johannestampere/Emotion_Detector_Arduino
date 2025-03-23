77da25667e1c0142e0743b04a24cb4116605fb84
Johannes Tampere <tamperejohannes@gmail.com>
2025-03-23 00:01:28 -0400

    ino ready

--- app.ino
+++ app.ino
@@ -15,5 +15,27 @@ void setup() {
 }
 
 void loop(
+    if (Serial.available() > 0) {
+        char emotion = Serial.read();
 
+        digitalWrite(happy, LOW);
+        digitalWrite(sad, LOW);
+        digitalWrite(angry, LOW);
+        digitalWrite(surprised, LOW);
+        digitalWrite(neutral, LOW);
+
+        if (emotion == 'h') {
+            digitalWrite(happy, HIGH);
+        } else if (emotion == 's') {
+            digitalWrite(sad, HIGH);
+        } else if (emotion == 'a') {
+            digitalWrite(angry, HIGH);
+        } else if (emotion == 'p') {
+            digitalWrite(surprised, HIGH);
+        } else if (emotion == 'n') {
+            digitalWrite(neutral, HIGH);
+        } else {
+            break;
+        }
+    }
 )
